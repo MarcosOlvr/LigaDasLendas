fca5b0f9bee3e2100d921d9ed829debc0324562a
Marcos <marquinhos.dev@gmail.com>
2022-09-12 16:37:33 -0300

    first commit

---
+++ League.Api/Controllers/ChampsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Camille.Enums;
+using Camille.RiotGames;
+
+namespace League.Api.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class ChampsController : ControllerBase
+    {
+        RiotGamesApi riotApi;
+
+        public ChampsController()
+        {
+            riotApi = RiotGamesApi.NewInstance(Settings.Key);
+        }
+
+        [HttpGet("masteries/{summonerName}")]
+        public ActionResult GetMasteriesChamps([FromRoute] string summonerName)
+        {
+            var summoner = riotApi.SummonerV4().GetBySummonerName(PlatformRoute.BR1, summonerName);
+            if (summoner == null)
+                return NotFound();
+
+            var masteries = riotApi.ChampionMasteryV4().GetTopChampionMasteries(PlatformRoute.BR1, summoner.Id);
+            
+            foreach (var champ in masteries)
+            {
+                Console.WriteLine(champ.ChampionId.ToString().ToUpperInvariant());
+            }
+
+            return Ok(masteries);
+        }
+
+        [HttpGet]
+        public ActionResult GetChamp(int id)
+        {
+            var champ = riotApi.ChampionV3().GetChampionInfo(PlatformRoute.BR1);
+
+            return Ok(champ);
+        }
+    }
+}
