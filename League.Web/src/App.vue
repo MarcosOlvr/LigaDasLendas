4697acebec387238f50d07dfd36fed009408391f
Marcos <marquinhos.dev@gmail.com>
2022-09-20 16:41:18 -0300

    add vue js framework

---
+++ League.Web/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import SearchSummoner from './components/SearchSummoner.vue';
+</script>
+
+<template>
+  <header>
+    <div>
+    </div>
+  </header>
+
+  <main>
+      <SearchSummoner></SearchSummoner>
+  </main>
+</template>
---
+++ League.Web/src/components/SearchSummoner.vue
@@ -0,0 +1,10 @@
+<script setup>  
+</script>
+
+<template>
+    <main>
+        <div>
+            <h1>teste DJHSALKJDNAJNKDA</h1>
+        </div>
+    </main>
+</template>
