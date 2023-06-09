26d40bd66266ee0db571fa4b4695005f416fb9b0
Marcos <marquinhos.dev@gmail.com>
2023-06-09 13:05:03 -0300

    continuando profile page

--- League.Web/src/components/SearchSummoner.vue
+++ League.Web/src/components/SearchSummoner.vue
@@ -9,7 +9,7 @@
             summoner: null,
             summonerIconUrl: "",
             masteries: [],
-            championsMasteries: [],
+            league: [],
         };
     },
     methods: {
@@ -23,6 +23,10 @@
 
             api.get(`/masteries/${this.summonerName}`)
             .then((response) => (this.masteries = response.data));
+
+            api.get(`summoner/${this.summonerName}`)
+            .then((response) => (api.get(`/league/${response.data.id}`)
+            .then((response) => (this.league = response.data[0]))));
         },
     },
     });
@@ -33,21 +37,41 @@
             <div>
                 <div v-if="summoner !== null">
                     <div class="row">
-                        <div class="d-inline-block col-4">
-                            <img class="m-3 rounded" :src="summonerIconUrl" height="100">
-                            <div class="d-flex">
-                                <h3>{{ summoner.name }}</h3>
+                        <div class="col-4 text-center border-end border-secondary">
+                            <img class="m-3 rounded shadow" :src="summonerIconUrl" height="100">
+                            <div class="d-flex justify-content-center mt-2">
+                                <h3 class="text-warning">{{ summoner.name }}</h3>
                                 <span class="mx-3 mt-2">
-                                    Nível <span class="text-warning">{{ summoner.summonerLevel }}</span>
+                                    Nível <span class="text-info">{{ summoner.summonerLevel }}</span>
                                 </span>
                             </div>
                         </div>
+                        <div v-if="league !== null" class="col-4 text-center border-end border-secondary">
+                            <img v-if="league.tier === 'IRON'" src="../services/imagens/IRON.png" height="125">
+                            <img v-if="league.tier === 'BRONZE'" src="../services/imagens/BRONZE.png" height="125">
+                            <img v-if="league.tier === 'SILVER'" src="../services/imagens/SILVER.png" height="125">
+                            <img v-if="league.tier === 'GOLD'" src="../services/imagens/GOLD.png" height="125">
+                            <img v-if="league.tier === 'PLATINUM'" src="../services/imagens/PLATINUM.png" height="125">
+                            <img v-if="league.tier === 'DIAMOND'" src="../services/imagens/DIAMOND.png" height="125">
+                            <img v-if="league.tier === 'MASTER'" src="../services/imagens/MASTER.png" height="125">
+                            <img v-if="league.tier === 'GRANDMASTER'" src="../services/imagens/GRANDMASTER.png" height="125">
+                            <img v-if="league.tier === 'CHALLENGER'" src="../services/imagens/CHALLENGER.png" height="125">
+                            <div class="d-flex justify-content-center mt-2"> 
+                                <p>{{ league.tier }}</p>
+                                <p class="ms-1">{{ league.rank }}</p>
+                            </div>
+                            <span class="text-white-50">{{ league.leaguePoints }} pdl</span>
+                        </div>
                         <div class="col-4">
-                            <div v-for="m in masteries">
-                                <div v-for="champ in championsMasteries">
-                                    <h1 v-if="m.championId === champ.id">{{ champ.name }}</h1>
+                            <div class="d-inline-block p-3 p-2 text-center" v-for="m in masteries">
+                                <div>
+                                    <img class="shadow" :src="m.champion.squareImage" height="75">
+                                    <h6 class="mt-2 text-warning">{{ m.champion.name }}</h6>
+                                    <span class="text-info">{{ m.championLevel }}</span>
+                                    <p>
+                                        <span class="text-muted">{{ m.championPoints }} pts</span> 
+                                    </p>
                                 </div>
-
                             </div>
                         </div>
                     </div>
@@ -55,9 +79,9 @@
                 </div>
             </div>
             <div v-if="summoner === null">
-                <img src="../services/imagens/download.jpg" width="500" class="mx-auto d-block mt-4 mb-3 img-responsive">
+                <img src="../services/imagens/download.jpg" width="500" class="mx-auto d-block mt-4 mb-3 img-responsive shadow">
                 <div class="input-group mb-3">
-                    <input type="text" v-model="summonerName" class="form-control text-white bg-dark border-1 border-secondary shadow-none" placeholder="Nome de Invocador"/>
+                    <input type="text" v-model="summonerName" class="form-control text-white bg-dark border-1 border-secondary shadow" placeholder="Nome de Invocador"/>
                     <button class="input-group-text btn btn-primary rounded-left border-1" @click="search()"><i class="bi bi-search"></i></button>
                 </div>
             </div> 
